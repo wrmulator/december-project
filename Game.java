756d8778f7d14a7a5158c0b533f5233430b8fa62
Nora <111463807+wrmulator@users.noreply.github.com>
2022-12-09 21:50:54 +0000

    12/9

--- Game.java
+++ Game.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 
 public class Game{
     Scanner in = new Scanner(System.in);
-    private ArrayList<Item> inv = new ArrayList<Item>();
-    private ArrayList<Item> ptr = new ArrayList<Item>();
-    private ArrayList<Item> stk = new ArrayList<Item>();
-    private ArrayList<Item> pot = new ArrayList<Item>();
-    private ArrayList<String> plc = new ArrayList<String>();
-    private String loc;
-    private int wlt = 10;
+    private ArrayList<Item> inv = new ArrayList<Item>(); //inventory
+    private ArrayList<Item> ptr = new ArrayList<Item>(); //pantry
+    private ArrayList<Item> stk = new ArrayList<Item>(); //shop stock
+    private ArrayList<Item> pot = new ArrayList<Item>(); //pot
+    private ArrayList<String> plc = new ArrayList<String>(); //locations (places)
+    private String loc; //current location
+    private int wlt = 10; //wallet
 
     public Game(){
         ptr.add(new Item("tooth", 1));
@@ -38,14 +38,14 @@ public class Game{
     }
 
     public void help(){
-        if (loc.equals(plc.get(0))){
+        if (loc.equals(plc.get(0))){ //if current location = pantry
             System.out.println("> You are in the pantry.");
             System.out.println("> Type 'check pantry' to check the contents of the pantry.");
             System.out.println("> Type 'take [item]' to move an item from the pantry to your inventory.");
             System.out.println("> Type 'leave [item]' to move an item from your inventory to the pantry.");
             System.out.println("> Items in the pantry can only be accessed from the pantry. Items in your inventory can be accessed from anywhere.");
             System.out.println("> Type 'go to [location]' to leave the pantry.");
-        } if (loc.equals(plc.get(1))){
+        } if (loc.equals(plc.get(1))){ //if current location = kitchen
             System.out.println("> You are in the kitchen.");
             System.out.println("> Type 'check pot' to check the contents of the pot.");
             System.out.println("> Type 'take [item]' to move an item from the pot to your inventory.");
@@ -53,7 +53,7 @@ public class Game{
             System.out.println("> Items in the pot can only be accessed from the kitchen. Items in your inventory can be accessed from anywhere.");
             System.out.println("> Type 'cook' to cook the contents of the pot.");
             System.out.println("> Type 'go to [location]' to leave the kitchen.");
-        } if (loc.equals(plc.get(2))){
+        } if (loc.equals(plc.get(2))){ //if current location = shop
             System.out.println("> You are in the shop.");
             System.out.println("> Type 'check stock' to check what items you can buy.");
             System.out.println("> Type 'check wallet' to check how much money you have.");
@@ -64,7 +64,7 @@ public class Game{
         cmd();
     }
 
-    public void cmd(){
+    public void cmd(){ //method that reads input
         String cmd = in.nextLine();
         if (cmd.equals("help"))
             help();
@@ -73,7 +73,9 @@ public class Game{
         if (cmd.equals("quit")){}
         if (cmd.equals("drink") || cmd.equals("drink potion") || cmd.equals("ingest potion") || cmd.equals("ingest"))
             drink();
-        String[] wrd = cmd.split(" ");
+        //above commands are simple; all others require specification of object
+        String[] wrd = cmd.split(" "); //if user input != any of the above commands, make it a list
+        //in order to analyze each part (command and object)
         if (wrd[0].equals("check")){
             if (wrd.length == 2)
                 check(wrd[1]);
@@ -81,10 +83,10 @@ public class Game{
                 System.out.println("> The 'check' command consists of two words: the command and the container.");
         } if (wrd[0].equals("go")){
             if (wrd.length == 1){
-                System.out.println("> ok");
-            } else if (wrd.length == 3 && wrd[1].equals("to")){
+                System.out.println("> ok"); //teehee
+            } else if (wrd.length == 3 && wrd[1].equals("to")){ //grammatical form: 'go to place'
                 go(wrd[2]);
-            } else if (wrd.length == 2) {
+            } else if (wrd.length == 2) { //'go place;' ungrammatical but recognizable
                 go(wrd[1]);
             } else {
                 System.out.println("> The 'go' command consists of two to three words: the command, the location, and the optional preposition 'to.'");
@@ -110,27 +112,29 @@ public class Game{
             else
                 System.out.println("> The 'sell' command consists of two words: the command and the item.");
         }
-        System.out.println("> Invalid command.");
+        System.out.println("> Invalid command."); //if none of the above, then not a valid command
+        //alert user to error & provide opportunity to correct it (request new input)
         cmd();
     }
 
     public void cook(){
-        if (loc.equals(plc.get(1))){
-            while (pot.size() > 0)
-                pot.remove(0);
+        if (loc.equals(plc.get(1))){ //if location = kitchen
+            while (pot.size() > 0) //empty the pot
+                pot.remove(0); 
             inv.add(new Item("potion", 20));
             System.out.println("> You have made a potion! Congratulations. Please do not ingest it.");
-            System.out.println("> 'Potion' was added to your inventory.");
+            System.out.println("> 'potion' was added to your inventory.");
             cmd();
-        }
+        } else
+            System.out.println("> You cannot cook outside of your kitchen.");
     }
 
     public void drink(){
-        boolean b = false;
+        boolean b = false; //does inventory contain potion?
         for (int x = 0; x < inv.size() && !b; x ++){
             if (inv.get(x).nm().equals("potion"))
                 b = true;
-        } if (b){
+        } if (b){ //if potion is in inventory
             System.out.println("> I told you not to do that.");
             System.exit(0);
         } else {
@@ -138,9 +142,9 @@ public class Game{
         }
     }
 
-    public void take(String s){
-        if (loc.equals(plc.get(0))){
-            boolean b = false;
+    public void take(String s){ //parameter: name of item to take
+        if (loc.equals(plc.get(0))){ //if location = pantry
+            boolean b = false; //is item in pantry?
             for (int x = 0; x < ptr.size(); x ++){
                 if (ptr.get(x).nm().equals(s)){
                     b = true;
@@ -151,8 +155,8 @@ public class Game{
                 }
             } if(!b)
                 System.out.println("> That item is not in this room or does not exist.");
-        } if (loc.equals(plc.get(1))){
-            boolean b = false;
+        } if (loc.equals(plc.get(1))){ //if location = kitchen
+            boolean b = false; //is item in pot?
             for (int x = 0; x < pot.size(); x ++){
                 if (ptr.get(x).nm().equals(s)){
                     b = true;
@@ -163,17 +167,21 @@ public class Game{
                 }
             } if (!b)
                 System.out.println("> That item is not in this room or does not exist.");
-        } if (loc.equals(plc.get(2))){
-            boolean b = false;
+        } if (loc.equals(plc.get(2))){ //if location = shop
+            boolean b = false; //is item in stock?
             for (int x = 0; x < stk.size(); x ++){
                 if (stk.get(x).nm().equals(s)){
                     b = true;
-                    inv.add(stk.get(x));
-                    wlt -= stk.get(x).prc();
-                    if (stk.get(x).prc() == 1)
-                        System.out.println("> '" + stk.get(x).nm() + "' was added to your inventory at the price of 1 dabloon.");
+                    Item i = stk.get(x);
+                    if (x < 5) //if item was part of original stock
+                        inv.add(stk.get(x)); //do NOT remove from stock
+                    else //if tem was sold to shop by player
+                        inv.add(stk.remove(x)); //remove from stock
+                    wlt -= i.prc();
+                    if (i.prc() == 1)
+                        System.out.println("> '" + i.nm() + "' was added to your inventory for the price of 1 dabloon.");
                     else
-                        System.out.println("> '" + stk.get(x).nm() + "' was added to your inventory at the price of " + stk.get(x).prc() + " dabloons.");
+                        System.out.println("> '" + i.nm() + "' was added to your inventory for the price of " + i.prc() + " dabloons.");
                     x = stk.size();
                 }
             } if (!b)
@@ -182,9 +190,9 @@ public class Game{
         cmd();
     }
 
-    public void leave(String s){
-        if (loc.equals(plc.get(0))){
-            boolean b = false;
+    public void leave(String s){ //parameter: name of item to leave
+        if (loc.equals(plc.get(0))){ //if location = pantry
+            boolean b = false; //is item in inventory?
             for (int x = 0; x < inv.size() && !b; x ++){
                 if (inv.get(x).nm().equals(s)){
                     Item i = inv.remove(x);
@@ -194,8 +202,8 @@ public class Game{
                 }
             } if (!b)
                 System.out.println("> That item is not in your inventory.");
-        } if (loc.equals(plc.get(1))){
-            boolean b = false;
+        } if (loc.equals(plc.get(1))){ //if location = kitchen
+            boolean b = false; //is item in inventory?
             for (int x = 0; x < inv.size() && !b; x ++){
                 if (inv.get(x).nm().equals(s)){
                     b = true;
@@ -206,28 +214,28 @@ public class Game{
                 }
             } if (!b)
                 System.out.println("> That item is not in your iventory.");
-        } if (loc.equals(plc.get(2))){
-            boolean b = false;
-            boolean c = false;
+        } if (loc.equals(plc.get(2))){ //if location = shop
+            boolean c = false; //is item in stock?
+            boolean b = false; //is item in inventory?
             for (int x = 0; x < stk.size(); x ++){
                 if (stk.get(x).nm().equals(s))
-                    b = true;
+                    c = true;
             }
             for (int x = 0; x < inv.size(); x ++){
                 if (inv.get(x).nm().equals(s)){
-                    c = true;
-                    if (b){
-                        Item i = inv.remove(x);
+                    b = true;
+                    if (c){ //if item is in both inventory and stock
+                        Item i = inv.remove(x); //do not add to stock
                         wlt += i.prc();
                         x = inv.size();
                         if (i.prc() == 1)
                             System.out.println("> '" + i.nm() + "' was removed from your inventory for the price of 1 dabloon.");
                         else
                             System.out.println("> '" + i.nm() + "' was removed from your inventory for the price of " + i.prc() + " dabloons.");
-                    } else {
+                    } else { //if item is in inventory but not stock
                         Item i = inv.remove(x);
                         wlt += i.prc();
-                        stk.add(i);
+                        stk.add(i); //add to stock
                         x = inv.size();
                         if (i.prc() == 1)
                             System.out.println("> '" + i.nm() + "' was removed from your inventory for the price of 1 dabloon.");
@@ -235,23 +243,23 @@ public class Game{
                             System.out.println("> '" + i.nm() + "' was removed from your inventory for the price of " + i.prc() + " dabloons.");
                     }
                 }
-            } if (!c)
-                System.out.println("> Thatitem is not in your inventory.");
+            } if (!b)
+                System.out.println("> That item is not in your inventory.");
         }
         cmd();
     }
 
-    public void go(String s){
+    public void go(String s){ //parameter: name of location to go
         if (s.equals("pantry")){
-            loc = plc.get(0);
+            loc = plc.get(0); //current location = place 0: pantry
             System.out.println("> You are in " + loc + ".");
             System.out.println("> Here you can access your stored ingredients.");
         } else if (s.equals("kitchen")){
-            loc = plc.get(1);
+            loc = plc.get(1); //current location = place 1: kitchen
             System.out.println("> You are in " + loc + ".");
             System.out.println("> Here you can cook ingredients in your pot.");
         } else if (s.equals("shop")){
-            loc = plc.get(2);
+            loc = plc.get(2); //current location = place 2: shop
             System.out.println("> You are in " + loc + ".");
             System.out.println("> Here you can buy new ingredients with which to cook.");
         } else
@@ -259,7 +267,7 @@ public class Game{
         cmd();
     }
 
-    public void check(String s){
+    public void check(String s){ //parameter: container to check
         if (s.equals("inventory")){
             if (inv.size() == 0){
                 System.out.println("> Your inventory is empty.");
