e0cdff58de46eea53281c0c1534df3942d95e8fa
Nora <111463807+wrmulator@users.noreply.github.com>
2022-12-12 22:03:18 +0000

    12/12

--- Game.java
+++ Game.java
@@ -13,13 +13,11 @@ public class Game{
 
     public Game(){
         ptr.add(new Item("tooth", 1));
-        // ptr.add(new Item("normal rock", 1));
         ptr.add(new Item("parsley", 2));
         stk.add(new Item("bone", 1));
         stk.add(new Item("quartz", 2));
         stk.add(new Item("rosemary", 2));
         stk.add(new Item("saffron", 14));
-        // stk.add(new Item("shiny rock", 5));
         stk.add(new Item("frog", 3));
         plc.add("your pantry");
         plc.add("your kitchen");
